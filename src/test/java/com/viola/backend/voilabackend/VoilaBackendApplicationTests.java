147a2675e1dd50eeacbce033fd069f067355da0c
Doruk Özdemir <dorukozdemir@gmail.com>
2022-03-02 14:43:33 +0300

    Initial commit

---
+++ src/test/java/com/viola/backend/voilabackend/VoilaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viola.backend.voilabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoilaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
