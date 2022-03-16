cdedccdeb160c553376d02f6d111eaf76e163aed
Doruk Özdemir <dorukozdemir@gmail.com>
2022-03-16 14:41:26 +0300

    Get için metot eklendi

--- src/main/java/com/viola/backend/voilabackend/helper/PostRequestHelper.java
+++ src/main/java/com/viola/backend/voilabackend/helper/PostRequestHelper.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.viola.backend.voilabackend.model.Request;
 import com.viola.backend.voilabackend.model.UserRequest;
 
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -54,5 +55,13 @@ public class PostRequestHelper {
         HttpEntity entity = response.getEntity();
         String responseString = EntityUtils.toString(entity, "UTF-8");
         return responseString;
+    }
+
+    public HttpResponse httpGet(String url, String jwt) throws IOException{
+        HttpGet request = new HttpGet(url);
+        request.addHeader("content-type", APPLICATION_JSON);
+        request.addHeader("Authorization", "Bearer " + jwt);
+        HttpResponse response = httpClient.execute(request);
+        return response;
     }
 }
