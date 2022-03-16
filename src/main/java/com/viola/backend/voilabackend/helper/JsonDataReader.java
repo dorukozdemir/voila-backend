fad197b93546b512ba733936f55c1ac16f4baac8
Doruk Özdemir <dorukozdemir@gmail.com>
2022-03-16 03:17:33 +0300

    Örnek json veri okuma yapısı geliştirildi

---
+++ src/main/java/com/viola/backend/voilabackend/helper/JsonDataReader.java
@@ -0,0 +1,31 @@
+package com.viola.backend.voilabackend.helper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import com.google.gson.Gson;
+import com.viola.backend.voilabackend.model.SocialMediaAccounts;
+
+import org.springframework.stereotype.Service;
+
+@Service("jsonDataReader")
+public class JsonDataReader {
+	private final String FOLDERPATH = "src/test/resources/com/viola/backend/voilabackend/data/";
+
+	public Object getSingleObjectFromFile(String file, Class clazz) {
+		Gson gson = new Gson();
+		String filePath = FOLDERPATH + file;
+		BufferedReader bufferReader = null;
+		try {
+			bufferReader = new BufferedReader(new FileReader(filePath));
+			Object object = gson.fromJson(bufferReader, clazz);
+			return object;
+		}catch(FileNotFoundException e) {
+			throw new RuntimeException("Json file not found at path : " + filePath);
+		}finally {
+			try { if(bufferReader != null) bufferReader.close();}
+			catch (IOException ignore) {}
+		}
+	}
+}
