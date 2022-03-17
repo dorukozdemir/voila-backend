c3686da701879deadce63754d3448566bf02449f
Doruk Özdemir <dorukozdemir@gmail.com>
2022-03-17 15:35:41 +0300

    Dosyadan json okuma eklendi

--- src/main/java/com/viola/backend/voilabackend/helper/JsonDataReader.java
+++ src/main/java/com/viola/backend/voilabackend/helper/JsonDataReader.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.viola.backend.voilabackend.model.SocialMediaAccounts;
 
 import org.springframework.stereotype.Service;
@@ -29,9 +33,26 @@ public class JsonDataReader {
 			bufferReader = new BufferedReader(new FileReader(filePath));
 			Object object = gson.fromJson(bufferReader, clazz);
 			return object;
-		}catch(FileNotFoundException e) {
+		} catch(FileNotFoundException e) {
 			throw new RuntimeException("Json file not found at path : " + filePath);
-		}finally {
+		} catch(JsonIOException e) {
+			throw new RuntimeException("Can not conert to object");
+		} finally {
+			try { if(bufferReader != null) bufferReader.close();}
+			catch (IOException ignore) {}
+		}
+	}
+
+	public String getJsonStringfromFile(String fileName) {
+		String filePath = FOLDERPATH + fileName;
+		BufferedReader bufferReader = null;
+		String result; 
+		try {
+			result = new String(Files.readAllBytes(Paths.get(filePath)));
+    		return result;  
+		} catch(IOException e) {
+			throw new RuntimeException("Json file not found at path : " + filePath);
+		} finally {
 			try { if(bufferReader != null) bufferReader.close();}
 			catch (IOException ignore) {}
 		}
