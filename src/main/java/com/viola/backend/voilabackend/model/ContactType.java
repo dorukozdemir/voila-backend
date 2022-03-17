808717d6f6a2a626476d0e9fb10ba21675fc2dad
Doruk Özdemir <dorukozdemir@gmail.com>
2022-03-17 15:07:32 +0300

    İletişim türlerine string bilgiler eklendi

--- src/main/java/com/viola/backend/voilabackend/model/ContactType.java
+++ src/main/java/com/viola/backend/voilabackend/model/ContactType.java
@@ -1,7 +1,18 @@
 package com.viola.backend.voilabackend.model;
 
 public enum ContactType {
-    WHATSAPP,
-    PHONE,
-    EMAIL
+    WHATSAPP("whatsapp"),
+    PHONE("phone"),
+    EMAIL("email");
+
+    private String text;
+
+    ContactType(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
 }
